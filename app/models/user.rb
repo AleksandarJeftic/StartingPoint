7107a928a2b4d70ac055a2c2e51d775db35ccfa1
AleksandarJeftic <aleksandar@axiomq.com>
2022-09-16 23:54:52 +0200

    Added actions and show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
             presence: true
 
   validates :email, uniqueness: true
+
+  has_many :user_permissions, dependent: :destroy
+  has_many :permissions, through: :user_permissions, dependent: :destroy
             
   enum status: { status_1: 'status_1', status_2: 'status_2' }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
